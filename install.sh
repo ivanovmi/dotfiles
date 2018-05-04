7164483b4feaa0dc90353bc293f6c0ed522392ef
Mikhail Ivanov <mivanov@mirantis.com>
2018-05-04 14:29:22 +0400

    Add new features
    - Add exesudo function (wrapper on sudo)
    - Add linuxbrew and brew packages installation
    - Add ngrok installation

--- install.sh
+++ install.sh
@@ -2,6 +2,102 @@
 set -xeu
 set -o pipefail
 
+# Little trick, should be rewrited
+exesudo() {
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# EXESUDO
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#
+# Purpose:
+# -------------------------------------------------------------------- #
+# Execute a function with sudo
+#
+# Params:
+# -------------------------------------------------------------------- #
+# $1:   string: name of the function to be executed with sudo
+#
+# Usage:
+# -------------------------------------------------------------------- #
+# exesudo "funcname" followed by any param
+#
+# -------------------------------------------------------------------- #
+# Created 01 September 2012              Last Modified 02 September 2012
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+    #
+    # LOCAL VARIABLES:
+    #
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+    #
+    # I use underscores to remember it's been passed
+    local _funcname_="$1"
+
+    local params=( "$@" )               ## array containing all params passed here
+    local tmpfile="/dev/shm/$RANDOM"    ## temporary file
+    local filecontent                   ## content of the temporary file
+    local regex                         ## regular expression
+    local func                          ## function source
+
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+    #
+    # MAIN CODE:
+    #
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+    #
+    # WORKING ON PARAMS:
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    #
+    # Shift the first param (which is the name of the function)
+    unset params[0]              ## remove first element
+    # params=( "${params[@]}" )     ## repack array
+
+
+    #
+    # WORKING ON THE TEMPORARY FILE:
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    content="#!/bin/bash\n\n"
+
+    #
+    # Write the params array
+    content="${content}params=(\n"
+
+    regex="\s+"
+    for param in "${params[@]}"
+    do
+        if [[ "$param" =~ $regex ]]
+            then
+                content="${content}\t\"${param}\"\n"
+            else
+                content="${content}\t${param}\n"
+        fi
+    done
+
+    content="$content)\n"
+    echo -e "$content" > "$tmpfile"
+
+    #
+    # Append the function source
+    echo "#$( type "$_funcname_" )" >> "$tmpfile"
+
+    #
+    # Append the call to the function
+    echo -e "\n$_funcname_ \"\${params[@]}\"\n" >> "$tmpfile"
+
+
+    #
+    # DONE: EXECUTE THE TEMPORARY FILE WITH SUDO
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    sudo bash "$tmpfile"
+    rm "$tmpfile"
+}
+
+#setup_dotfiles() {}
+
 setup_repos() {
   set +x
   repo_dir=$1
@@ -25,11 +121,29 @@ install_packages() {
   fi
 }
 
+setup_brew() {
+  brew_pkgs_list=$1
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
+  for pkg in $(cat $brew_pkgs_list); do
+    brew install $pkg
+  done
+}
+
+setup_ngrok() {
+  mkdir -p $HOME/bin
+  wget -O /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+  unzip /tmp/ngrok.zip -d $HOME/bin
+}
+
 #install_pip_packages() {}
 
 #install_gem_packages() {}
 
-setup_repos ./repos_list
-install_packages
+#setup_dotfiles
+exesudo setup_repos ./repos_list
+exesudo install_packages
 #install_pip_packages
 #install_gem_packages
+setup_brew ./linuxbrew
+setup_ngrok
