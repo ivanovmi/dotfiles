9711f6c53ffcc0e68ad0071308469f5d961fdec8
Mikhail Ivanov <mivanov@mirantis.com>
2018-10-04 19:12:14 +0400

    add box function

--- scripts/common.sh
+++ scripts/common.sh
@@ -6,3 +6,5 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
 function e_dot()      { echo -e " \033[1;34m•\033[0m  $@"; }
+
+function box() { t="$1xxxx";c=${2:--}; b=${3:-|}; echo ${t//?/$c}; echo "$b $1 $b"; echo ${t//?/$c}; }
